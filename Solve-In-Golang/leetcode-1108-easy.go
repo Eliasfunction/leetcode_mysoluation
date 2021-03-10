baaa559575353d647641766fff75bef068c2873c
Eliasfunction <78813618+Eliasfunction@users.noreply.github.com>
2021-03-11 03:05:05 +0800

    Update leetcode-1108-easy.go

--- Solve-In-Golang/leetcode-1108-easy.go
+++ Solve-In-Golang/leetcode-1108-easy.go
@@ -1,17 +1,8 @@
-//package main
 import (
 	"strings"
 )
 
 func defangIPaddr(address string) string {
-	/*
   new_string := strings.Replace(address, ".", "[.]", -1)
   return new_string
-  */
-	return strings.Replace(address, ".", "[.]", -1)
 }
-/*
-func main(){
-
-}
-*/
